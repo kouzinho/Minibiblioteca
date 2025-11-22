0fd8a218420cbfaa263fdc546062211871113b9e
kouzinho <kouzepekeno@gmail.com>
2025-11-22 13:24:18 +0100

    Minibiblioteca v1.1(vista dinámica con /add)

--- src/main/java/com/example/Minibiblioteca/controller/MinibibliotecaController.java
+++ src/main/java/com/example/Minibiblioteca/controller/MinibibliotecaController.java
@@ -4,24 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MinibibliotecaController {
 
+    // Creadión de una lista en memoria para almacenar los libros como un String
+    private List<String> libros = new ArrayList<>();
+
+    // Ruta principal para mostrar la lista de libros
     @GetMapping("/")
     public String verLibros(Model model){
-        List<String> libros = Arrays.asList(
-            "El nombre del viento - Patrick Rothfuss",
-            "El capitán Alatriste - Pérez Reverte",
-            "Berserk - Kentaro Miura"
-        );
+        // Se agrega la lista al modelo
+        model.addAttribute("libros", libros);
+        // Se devuelve la plantilla listView.html
+        return "listView";
+    }
 
-        model.addAttribute("libros",libros);
+    // Ruta para agregar un libro con título y autor
+    @GetMapping("/add")
+    public String addLibro(@RequestParam String titulo, @RequestParam String autor, Model model) {
+        String libro = titulo + " - " + autor;
+        
+        // Se agrega el libro a la lista
+        libros.add(libro);
+
+        // Se pasa la lista actualizada a la vista
+        model.addAttribute("libros", libros);
 
         return "listView";
-        
     }
 }
